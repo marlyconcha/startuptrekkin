0efdd1d4b1e9c7c4fd0a50d3c285a1d763a22fce
Marly Concha <marly.concha@gmail.com>
2020-02-12 11:25:31 -0300

    navbar arreglado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += "responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+} 
 
 
